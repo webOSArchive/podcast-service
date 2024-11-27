97101d4b93164e55aebebb4ceb7a302400710172
codepoet <info@codepoetry.app>
2024-11-27 12:44:27 -0500

    restore PalmCast (again)

--- restorations.php
+++ restorations.php
@@ -10,5 +10,15 @@ $restorations = array(
         'description' => "Revisit memorable performances of Garrison Keillor's signature monologue, The News from Lake Wobegon, from rebroadcasts of the live public radio program A Prairie Home Companion. Garrison created A Prairie Home Companion in July 1974 and hosted for 42 years, until July 2016. Enjoy classic tales from the town where 'all the women are strong, all the men are good-looking, and all the children are above average.'",
         'substitution_reason' => "This feed was restored from a Wayback Machine archive. The original content links are unchanged."
     ],
+    "PalmCast (Restored)" => (array) [
+        'author' => "Mobile Nations",
+        'link' => "http://webosarchive.org",
+        'episodeCount' => 97,
+        'title' => "PalmCast (Restored)",
+        'url' => "http://www.webosarchive.org/podcasts/feeds/PalmCast/feed.xml",
+        'image' => "https://www.webosarchive.org/podcasts/feeds/PalmCast/PalmCast.jpg",
+        'description' => "Derek Kessler and team discuss the latest HP mobile news, offer webOS tips, and chime in with the PreCentral Community'",
+        'substitution_reason' => "This feed and its content links were restored from a Wayback Machine archive and hosted by the webOS Archive community. Some episodes are missing."
+    ],
 );
 ?>
